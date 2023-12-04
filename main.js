89ece41594d26dde11abfe1a2d50eff65e790b03
Ignat Matei-Marius <mateiignat03@gmail.com>
2023-12-04 20:16:58 +0200

    started working on text style

--- main.js
+++ main.js
@@ -42,7 +42,6 @@ function addStar() {
   const material = new THREE.MeshStandardMaterial({ 
     color: 0xffffff});
   const star = new THREE.Mesh(geometry, material);
-
   const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
   star.position.set(x, y, z);
   scene.add(star);
